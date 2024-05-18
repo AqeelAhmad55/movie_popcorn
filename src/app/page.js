a23ae3a4c548dd56943b1cdd711aae7181b90be6
MasterrOfEvil <aqeel14252@gmail.com>
2024-05-18 12:20:29 +0500

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,7 @@
 import Main from "@/components/main";
-import StarRating from "@/components/starRating";
-
 export default function Home() {
   return (
     <div>
-      {/* <StarRating maxRating={10} /> */}
       <Main />
     </div>
   );
--- src/customHooks/useLocalStorageState.jsx
+++ src/customHooks/useLocalStorageState.jsx
@@ -1,16 +1,31 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorageState(initialState, key) {
-  const [value, setValue] = useState(function () {
-    const storedValue = localStorage.getItem(key);
-    return storedValue ? JSON.parse(storedValue) : initialState;
+  const isClient = typeof window !== "undefined";
+
+  const [value, setValue] = useState(() => {
+    if (isClient) {
+      try {
+        const storedValue = localStorage.getItem(key);
+        return storedValue ? JSON.parse(storedValue) : initialState;
+      } catch (error) {
+        console.error("Error parsing localStorage value", error);
+        return initialState;
+      }
+    } else {
+      return initialState;
+    }
   });
 
-  useEffect(
-    function () {
-      localStorage.setItem(key, JSON.stringify(value));
-    },
-    [value, key]
-  );
+  useEffect(() => {
+    if (isClient) {
+      try {
+        localStorage.setItem(key, JSON.stringify(value));
+      } catch (error) {
+        console.error("Error setting localStorage value", error);
+      }
+    }
+  }, [value, key, isClient]);
+
   return [value, setValue];
 }
