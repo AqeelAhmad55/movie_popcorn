f2e0182e0988da9ec77d1702da307007a02c8dad
MasterrOfEvil <aqeel14252@gmail.com>
2024-05-19 15:50:54 +0500

    intials

--- src/customHooks/useLocalStorageState.jsx
+++ src/customHooks/useLocalStorageState.jsx
@@ -1,31 +1,18 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorageState(initialState, key) {
-  const isClient = typeof window !== "undefined";
-
-  const [value, setValue] = useState(() => {
-    if (isClient) {
-      try {
-        const storedValue = localStorage.getItem(key);
-        return storedValue ? JSON.parse(storedValue) : initialState;
-      } catch (error) {
-        console.error("Error parsing localStorage value", error);
-        return initialState;
-      }
-    } else {
-      return initialState;
-    }
+  const [value, setValue] = useState(function () {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialState;
   });
 
-  useEffect(() => {
-    if (isClient) {
-      try {
-        localStorage.setItem(key, JSON.stringify(value));
-      } catch (error) {
-        console.error("Error setting localStorage value", error);
-      }
-    }
-  }, [value, key, isClient]);
-
+  useEffect(
+    function () {
+      typeof window !== "undefined"
+        ? localStorage.setItem(key, JSON.stringify(value))
+        : null;
+    },
+    [value, key]
+  );
   return [value, setValue];
 }
